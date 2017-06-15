1670a2ae361545b297bf447bde8052188804457c
alex-tom <alexey.tomskih@gmail.com>
2017-06-15 18:10:57 +0700

    переделано всплывающее окно, сделан крестик удаления, внешний вид приведён в порядок, задействован Css

--- ldap/Scripts/myScript.js
+++ ldap/Scripts/myScript.js
@@ -1,7 +1,7 @@
 ﻿function showWnd() {
     $('#dialogDiv')
         .load('/Logic/SomeAction/')
-        .dialog({autoOpen: true, modal: true, width: 400, height:300, title: 'Добавить новое мероприятие', resizable: false });
+        .dialog({autoOpen: true, modal: true, width: 400, height:300, title: 'Новое мероприятие', resizable: false });
 };
 
 
